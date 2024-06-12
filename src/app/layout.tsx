48008b59865b46938085befbb9785b30413fc58b
Igor PC <igor_roc@hotmail.com.br>
2024-06-11 22:30:43 -0300

    fix: Change font to Montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,39 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Montserrat } from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const font = Montserrat({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-	title: {
-		default: "Velltri - Soluções em Inteligência Artificial para Transformar Negócios",
-		template: "%s | Velltri",
-	},
-	description:
-		"Desenvolvemos soluções inovadoras utilizando inteligência artificial para transformar e melhorar empresas. Descubra como nossas soluções podem impulsionar seu negócio.",
-	keywords: [
-		"Inteligência Artificial",
-		"Machine Learning",
-		"Soluções em IA",
-		"Transformação Digital",
-		"Desenvolvimento de Software",
-		"Consultoria em IA",
-		"Data Science",
-		"Big Data",
-		"UI/UX",
-		"Análise de Dados",
-	],
+    title: {
+        default:
+            "Velltri - Soluções em Inteligência Artificial para Transformar Negócios",
+        template: "%s | Velltri",
+    },
+    description:
+        "Desenvolvemos soluções inovadoras utilizando inteligência artificial para transformar e melhorar empresas. Descubra como nossas soluções podem impulsionar seu negócio.",
+    keywords: [
+        "Inteligência Artificial",
+        "Machine Learning",
+        "Soluções em IA",
+        "Transformação Digital",
+        "Desenvolvimento de Software",
+        "Consultoria em IA",
+        "Data Science",
+        "Big Data",
+        "UI/UX",
+        "Análise de Dados",
+    ],
 }
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode
+    children: React.ReactNode
 }>) {
-	return (
-		<html lang="pt-br">
-			<body className={inter.className}>{children}</body>
-		</html>
-	)
+    return (
+        <html lang="pt-br">
+            <body className={font.className}>{children}</body>
+        </html>
+    )
 }
