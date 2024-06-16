7a0e3dd3a295a110f3f1010f8373390f276061b3
Igor PC <igor_roc@hotmail.com.br>
2024-06-16 18:40:53 -0300

    fix: Mobile down

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -1,6 +1,6 @@
 export default function MainWrapper(props: { children: React.ReactNode }) {
     return (
-        <main className="mx-auto flex h-dvh w-dvw max-w-[1500px] gap-16 p-4 lg:p-16">
+        <main className="mx-auto flex h-dvh w-dvw max-w-[1500px] gap-16 p-4 pt-48 transition-all sm:pt-4 md:p-8 lg:p-16">
             {props.children}
         </main>
     )
