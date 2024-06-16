01399b4e4a037ca35da46709f60c900bae9a1b42
Igor PC <igor_roc@hotmail.com.br>
2024-06-16 17:39:20 -0300

    fix: Small card item description

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -7,7 +7,7 @@ export default function CardItem(props: CardItemProps) {
     return (
         <div className="flex w-full min-w-[300px] flex-col gap-2 rounded-xl bg-white/10 p-8 shadow-lg backdrop-blur-lg">
             <h3 className="text-lg font-bold lg:text-2xl">{props.title}</h3>
-            <p className="text-xs font-light lg:text-medium">
+            <p className="text-sm font-light lg:text-medium">
                 {props.description}
             </p>
         </div>
